405fd33ac02b2b83cbc62acecb67d86e649a56fc
MBenincasa <mirkobenincasa44@gmail.com>
2024-02-26 19:07:07 +0100

    In the AWS Lambda that returns data for the monthly trend, pollution data is now also present

--- aws-dynamoDB/aws-lambda/read-raspberry-dht22-data/lambda_function.py
+++ aws-dynamoDB/aws-lambda/read-raspberry-dht22-data/lambda_function.py
@@ -17,7 +17,37 @@ def scan_dynamodb_table(table):
         items.extend(response['Items'])
     return items
 
-def aggregate_data(items):
+def aggregate_pollution_data(items):
+    aggregated_data = defaultdict(lambda: {"pm2_5_city_sum": 0, "pm10_city_sum": 0, "no2_city_sum": 0, "count": 0})
+    for item in items:
+        date = item.get('date')
+        pm2_5_city = item.get('pm2_5_city')
+        pm10_city = item.get('pm10_city')
+        no2_city = item.get('no2_city')
+        
+        if all(v is not None for v in [date, pm2_5_city, pm10_city, no2_city]):
+            aggregated_data[date]["pm2_5_city_sum"] += pm2_5_city
+            aggregated_data[date]["pm10_city_sum"] += pm10_city
+            aggregated_data[date]["no2_city_sum"] += no2_city
+            aggregated_data[date]["count"] += 1
+    return aggregated_data
+
+def calculate_pollution_averages(aggregated_data):
+    result_list = []
+    for date, data in aggregated_data.items():
+        pm2_5_city_avg = round(data["pm2_5_city_sum"] / data["count"], 2) if data["count"] > 0 else None
+        pm10_city_avg = round(data["pm10_city_sum"] / data["count"], 2) if data["count"] > 0 else None
+        no2_city_avg = round(data["no2_city_sum"] / data["count"], 2) if data["count"] > 0 else None
+        
+        result_list.append({
+            "date": date,
+            "pm2_5_city_avg": pm2_5_city_avg,
+            "pm10_city_avg": pm10_city_avg,
+            "no2_city_avg": no2_city_avg
+        })
+    return result_list
+
+def aggregate_weather_data(items):
     aggregated_data = defaultdict(lambda: {"temperature_sensor_sum": 0, "humidity_sensor_sum": 0, "temperature_city_sum": 0, "humidity_city_sum": 0, "count": 0})
     for item in items:
         date = item.get('date')
@@ -34,7 +64,7 @@ def aggregate_data(items):
             aggregated_data[date]["count"] += 1
     return aggregated_data
 
-def calculate_averages(aggregated_data):
+def calculate_weather_averages(aggregated_data):
     result_list = []
     for date, data in aggregated_data.items():
         temperature_sensor_avg = round(data["temperature_sensor_sum"] / data["count"], 2) if data["count"] > 0 else None
@@ -64,25 +94,40 @@ def lambda_handler(event, context):
     table = dynamodb.Table(dynamodb_table_name)
 
     items = scan_dynamodb_table(table)
-    aggregated_data = aggregate_data(items)
-    result_list = calculate_averages(aggregated_data)
+    aggregated_weather_data = aggregate_weather_data(items)
+    result_weather_list = calculate_weather_averages(aggregated_weather_data)
     
-    grouped_results = defaultdict(list)
-    for item in result_list:
+    grouped_weather_results = defaultdict(list)
+    for item in result_weather_list:
         year_month = item['date'][:7]
-        grouped_results[year_month].append(item)
+        grouped_weather_results[year_month].append(item)
 
-    response_data = []
-    for year_month, data in grouped_results.items():
-        data_sorted = sorted(data, key=lambda x: datetime.strptime(x['date'], '%Y-%m-%d'))
-        response_data.append({
+    aggregated_pollution_data = aggregate_pollution_data(items)
+    result_pollution_list = calculate_pollution_averages(aggregated_pollution_data)
+
+    grouped_pollution_results = defaultdict(list)
+    for item in result_pollution_list:
+        year_month = item['date'][:7]
+        grouped_pollution_results[year_month].append(item)
+    
+    data = []
+    for year_month in sorted(set(grouped_weather_results.keys()) | set(grouped_pollution_results.keys())):
+        weather_aggregation = grouped_weather_results[year_month]
+        pollution_aggregation = grouped_pollution_results[year_month]
+        
+        weather_aggregation = sorted(weather_aggregation, key=lambda x: datetime.strptime(x['date'], '%Y-%m-%d'))
+        pollution_aggregation = sorted(pollution_aggregation, key=lambda x: datetime.strptime(x['date'], '%Y-%m-%d'))
+        
+        if not pollution_aggregation:
+            pollution_aggregation = None
+        
+        data.append({
             "date": year_month,
-            "aggregation": data_sorted
+            "weather_aggregation": weather_aggregation,
+            "pollution_aggregation": pollution_aggregation
         })
-    
-    response_data = sorted(response_data, key=lambda x: datetime.strptime(x['date'] + '-01', '%Y-%m-%d'))
-    
+
     return {
         'statusCode': 200,
-        'body': {"weather_data": response_data}
+        'body': {"data": data}
     }
